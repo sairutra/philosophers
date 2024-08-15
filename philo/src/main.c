1c7a6e2f5d961a829dc427e8673548fc8629cac4
spenning <whimizera@mailbox.org>
2024-08-15 17:31:44 +0200

    integer overflow fix

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: spenning <spenning@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/08/13 14:04:28 by spenning      #+#    #+#                 */
-/*   Updated: 2024/08/15 17:14:28 by spenning      ########   odam.nl         */
+/*   Updated: 2024/08/15 17:31:26 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,21 @@ int	philo_len(const char *arg)
 
 int	philo_strncmp(const char *s1, const char *s2, size_t n)
 {
+	int		retvalue;
 	size_t	index;
 	size_t	s1_len;
 	size_t	s2_len;
 
+	retvalue = 0;
 	index = 0;
 	s1_len = philo_len(s1);
 	s2_len = philo_len(s2);
 	while (index < n && (index < s1_len || index < s2_len))
 	{
-		if ((unsigned char)s1[index] != (unsigned char)s2[index])
-			return ((unsigned char)s1[index] - (unsigned char)s2[index]);
+		retvalue += ((unsigned char)s1[index] - (unsigned char)s2[index]);
 		index++;
 	}
-	return (0);
+	return (retvalue);
 }
 void print_err(char * msg)
 {
@@ -56,6 +57,11 @@ int	philo_atoi(char *arg)
 
 	index = 0;
 	result = 0;
+	if (philo_len(arg) > 10)
+	{
+		print_err("integer overflow: ");
+		return (-1);
+	}
 	if (philo_strncmp(arg, "2147483647", 10) > 0 && philo_len(arg) == 10)
 	{
 		print_err("integer overflow: ");
